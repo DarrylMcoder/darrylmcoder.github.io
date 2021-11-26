f40b259410b61b53da4016c19c4a6c3cadaed514
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-11-26 12:43:01 -0500

    Add files via upload

--- serviceworker/sw.js
+++ serviceworker/sw.js
@@ -1,3 +1,11 @@
+self.addEventListener('install', function(event){
+	console.log(event);
+});
+
+self.addEventListener('activate', function(event){
+    console.log(event);
+});
+
 self.addEventListener('fetch', function(event) {          
   if (/\.png$/.test(event.request.url)) {                 
     event.respondWith(fetch('/images/unicorn.png'));      
