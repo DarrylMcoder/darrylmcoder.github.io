4a47f5df56a6ae18c285dbe610a01843afee7dc6
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-12-07 20:02:17 -0500

    Add files via upload

--- proxytest/h/sw.js
+++ proxytest/h/sw.js
@@ -1,1 +1,49 @@
+self.addEventListener('install', function(event){
+      // Skip over the "waiting" lifecycle state, to ensure that our
+  // new service worker is activated immediately, even if there's
+  // another tab open controlled by our older service worker code.
+    self.skipWaiting();
+	console.log(event);
+});
 
+self.addEventListener('activate', function(event){
+    console.log(event);
+});
+
+self.addEventListener('fetch', function(event) {
+  if(event.request.url.includes("/darrylmcoder-proxy/")){
+    event.respondWith(handleRequest(event.request));
+  }else{
+    return;
+  }
+});
+    
+async function handleRequest(request) {
+  const response = await fetch(await editRequest(request));
+  return response;
+}
+
+async function editRequest(request) {
+  var proxy = "./?url=";
+  var url = proxy + await getRealUrl(request.url);
+  if(true) {
+    const newRequest = new Request(url, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+      credentials: request.credentials,
+      cache: request.cache,
+      redirect: request.redirect,
+      referrer: request.referrer,
+      integrity: request.integrity
+    });
+    return newRequest;
+  }else{
+   
+  }
+}
+
+async function getRealUrl(url) {
+  const parts = url.split("/darrylmcoder-proxy/");
+  return parts[1].trim();
+}
