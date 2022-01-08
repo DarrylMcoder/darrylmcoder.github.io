e79e2ed5840f2931d938701849ffa613052bcf92
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-01-07 21:25:56 -0500

    Add files via upload

--- proxytest/A/sw.js
+++ proxytest/A/sw.js
@@ -1,1 +1,32 @@
+self.importScripts('caesarShift.js');
+self.importScripts('https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js');
+self.importScripts('unblocker.js');
+self.importScripts('decodeContent.js');
 
+self.addEventListener('install', function(event){
+      // Skip over the "waiting" lifecycle state, to ensure that our
+  // new service worker is activated immediately, even if there's
+  // another tab open controlled by our older service worker code.
+    self.skipWaiting();
+	console.log(event);
+});
+
+self.addEventListener('activate', function(event){
+    console.log(event);
+});
+
+self.addEventListener('fetch',unblocker({
+    requestMiddleware: [
+      
+    ],
+    responseMiddleware: [
+      decodeContent({
+        processContentTypes: [
+          "text/html",
+          "text/javascript",
+          "text/css",
+        ],
+      }),
+      
+    ],
+  }));
